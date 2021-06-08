455601c30408c9e39f2a2665d9920513f0852306
iMarishin <move_up@icloud.com>
2021-06-08 13:33:22 +0500

    add some person

--- ContactList/Person.swift
+++ ContactList/Person.swift
@@ -21,11 +21,13 @@ struct Person {
 extension Person {
     static func getPersonList () -> [Person] {
         [
-            Person(firstName: "Alexander", lastName: "Pushkin", phone: 2355647, email: "onegin@mail.ru"),
+            Person(firstName: "Alexander", lastName: "Pushkin", phone: 2355647, email: "ladykiller@mail.ru"),
             Person(firstName: "Lev", lastName: "Tolstoy", phone: 2364577, email: "war&peace@mail.ru"),
             Person(firstName: "Nikolay", lastName: "Gogol", phone: 3463422, email: "deadSoul@mail.ru"),
             Person(firstName: "Mikhail", lastName: "Bulgakov", phone: 1246754, email: "master@mail.ru"),
             Person(firstName: "Mikhail", lastName: "Lermontov", phone: 5646654, email: "borodino@mail.ru"),
+            Person(firstName: "Sergey", lastName: "Esenin", phone: 2358738, email: "badboy@mail.ru"),
+            Person(firstName: "Vladimir", lastName: "Mayakovsky", phone: 2345656, email: "pasportman@mail.ru")
         ]
     }
 }

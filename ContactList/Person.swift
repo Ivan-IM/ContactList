a0fe8e9e514e52a60148b0f37d16467d613bebac
iMarishin <move_up@icloud.com>
2021-06-08 11:38:58 +0500

    init commit

---
+++ ContactList/Person.swift
@@ -0,0 +1,31 @@
+//
+//  Person.swift
+//  ContactList
+//
+//  Created by Иван Маришин on 08.06.2021.
+//
+
+import Foundation
+
+struct Person {
+    let firstName: String
+    let lastName: String
+    let phone: Int
+    let email: String
+    
+    var fullName: String {
+        "\(firstName) \(lastName)"
+    }
+}
+
+extension Person {
+    static func getPersonList () -> [Person] {
+        [
+            Person(firstName: "Alexander", lastName: "Pushkin", phone: 2355647, email: "onegin@mail.ru"),
+            Person(firstName: "Lev", lastName: "Tolstoy", phone: 2364577, email: "war&peace@mail.ru"),
+            Person(firstName: "Nikolay", lastName: "Gogol", phone: 3463422, email: "deadSoul@mail.ru"),
+            Person(firstName: "Mikhail", lastName: "Bulgakov", phone: 1246754, email: "master@mail.ru"),
+            Person(firstName: "Mikhail", lastName: "Lermontov", phone: 5646654, email: "borodino@mail.ru"),
+        ]
+    }
+}
